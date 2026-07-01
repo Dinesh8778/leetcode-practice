d57992b50c20fa6314d8ec7513e60898a1cda27e
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-01 11:06:04 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/Maximum Product of Word Lengths/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public int maxProduct(String[] words) {
+        int max = 0;
+        int len = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (checkLetter(words[i], words[j])) {
+                    len = words[i].length() * words[j].length();
+                    System.out.println(words[i] + " " + words[j] + " " + len);
+                }
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+
+    boolean checkLetter(String s, String t) {
+        boolean[] freq1 = new boolean[26];
+        boolean[] freq2 = new boolean[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq1[s.charAt(i) - 'a'] = true;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            freq2[t.charAt(i) - 'a'] = true;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] && freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

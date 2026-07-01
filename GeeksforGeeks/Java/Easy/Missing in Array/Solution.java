717a34f2b2943da05ca4da5d1418c77ee45fa060
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-01 10:45:49 +0530

    Add missing number calculation in Solution.java
    Implement logic to find the missing number in the array.

--- GeeksforGeeks/Java/Easy/Missing in Array/Solution.java
+++ GeeksforGeeks/Java/Easy/Missing in Array/Solution.java
@@ -1,6 +1,11 @@
 class Solution {
     int missingNum(int arr[]) {
-        // code here
-        
+        int n = arr.length + 1;
+
+        long sum = (long) n * (n + 1) / 2;
+        for (int i : arr) {
+            sum -= i;
+        }
+        return (int) sum;
     }
 }

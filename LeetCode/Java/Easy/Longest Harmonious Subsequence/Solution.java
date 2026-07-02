3d8d1e3b6758588dd82632971b423bb7e991eaa2
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-02 11:11:23 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/Longest Harmonious Subsequence/Solution.java
+++ LeetCode/Java/Easy/Longest Harmonious Subsequence/Solution.java
@@ -2,19 +2,20 @@ class Solution {
     public int findLHS(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i : nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
 
-        if(map.size() < 2){
+        if (map.size() < 2) {
             return 0;
         }
 
         int max = 0;
 
-        for(int i : map.keySet()){
-            if(map.containsKey(i+1))
-                max = Math.max(max,(map.get(i)+map.get(i+1)));
+        for (int i : map.keySet()) {
+            if (map.containsKey(i + 1)) {
+                max = Math.max(max, (map.get(i) + map.get(i + 1)));
+            }
         }
 
         return max;

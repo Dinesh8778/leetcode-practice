cb698008ee7cb717f294c1dc56c9a9bbe6d77ee2
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-02 12:43:56 +0530

    Add Basic Java Solution from GeeksforGeeks

---
+++ GeeksforGeeks/Java/Basic/Value equal to position/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> valueEqualToIndex(List<Integer> nums) {
+        List<Integer> ans = new ArrayList<>();
+        
+        for (int i = 0; i<nums.size(); i++) {
+            if (nums.get(i) == i + 1)
+                ans.add(i + 1);
+        }
+        
+        return ans;
+        
+    }
+}

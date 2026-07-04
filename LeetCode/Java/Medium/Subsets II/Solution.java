93c8f11d7f2f606bfb8b3d9ad7247f6a9f764c23
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-04 19:43:20 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/Subsets II/Solution.java
+++ LeetCode/Java/Medium/Subsets II/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        result.add(new ArrayList<>());
+        for (int num : nums) {
+            int n = result.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> in = new ArrayList<>(result.get(i));
+                in.add(num);
+                if (!result.contains(in)) {
+                    result.add(in);
+                }
+            }
+        }
+        // result.sort(Comparator.comparing(a -> a.get(0)));
+
+        return result;
     }
 }

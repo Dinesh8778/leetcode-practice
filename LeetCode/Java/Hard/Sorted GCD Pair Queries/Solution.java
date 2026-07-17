85a76792b347146cfce03f87da63eb65f389cc21
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-17 21:13:43 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/Sorted GCD Pair Queries/Solution.java
+++ LeetCode/Java/Hard/Sorted GCD Pair Queries/Solution.java
@@ -1,23 +1,69 @@
 class Solution {
     public int[] gcdValues(int[] nums, long[] queries) {
-        ArrayList<Integer> gcdPairs = new ArrayList();
-        for(int  i = 0 ; i < nums.length ; i++){
-            for(int j = i+1 ; j < nums.length ; j++){
-                gcdPairs.add(GCD(nums[i],nums[j]));
+
+        int max = 0;
+        for (int x : nums) {
+            max = Math.max(max, x);
+        }
+
+        // Frequency of each number
+        int[] freq = new int[max + 1];
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+        // Count numbers divisible by each gcd candidate
+        long[] divisible = new long[max + 1];
+
+        for (int g = 1; g <= max; g++) {
+            for (int multiple = g; multiple <= max; multiple += g) {
+                divisible[g] += freq[multiple];
             }
         }
-        Collections.sort(gcdPairs);
-        int[] ans = new int[queries.length];
-        int k = 0;
-        for(long i : queries){
-            int j = (int) i;
-            ans[k++] = gcdPairs.get(j);
+
+        // Count pairs having gcd exactly g
+        long[] exact = new long[max + 1];
+
+        for (int g = max; g >= 1; g--) {
+
+            long count = divisible[g];
+            exact[g] = count * (count - 1) / 2;
+
+            for (int multiple = 2 * g; multiple <= max; multiple += g) {
+                exact[g] -= exact[multiple];
+            }
         }
+
+        // Prefix sum of pair counts
+        long[] prefix = new long[max + 1];
+
+        for (int g = 1; g <= max; g++) {
+            prefix[g] = prefix[g - 1] + exact[g];
+        }
+
+        // Answer queries
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+
+            long target = queries[i] + 1;
+
+            int left = 1;
+            int right = max;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (prefix[mid] >= target) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            ans[i] = left;
+        }
+
         return ans;
     }
-
-    static int GCD(int a, int b){
-        if(b == 0) return a;
-        return GCD(b,a%b);
-    } 
 }

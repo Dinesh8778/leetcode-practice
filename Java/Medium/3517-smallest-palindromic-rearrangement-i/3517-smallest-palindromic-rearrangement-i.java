5477784d70047bfaa28e093d08cc7a0aea8c4b53
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-28 14:34:09 +0530

    3517-smallest-palindromic-rearrangement-i String Medium .java

---
+++ Java/Medium/3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public String smallestPalindrome(String s) {
+        int len = s.length();
+        int mid = len / 2;
+
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr, 0, mid);
+
+        for (int i = 0; i < mid; i++) {
+            arr[len - 1 - i] = arr[i];
+        }
+
+        return new String(arr);
+    }
+}
